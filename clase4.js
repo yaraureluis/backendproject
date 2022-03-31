347aff946bad749d1dfae6fbbd1fe03448b5aaba
yaraureluis <yaraureluis@gmail.com>
2022-03-31 17:45:53 -0300

    Se agregó el archivo clase4.js

---
+++ clase4.js
@@ -0,0 +1,106 @@
+const fs = require("fs");
+
+class Contenedor {
+  constructor(ruta) {
+    this.ruta = ruta;
+    this.productos = [];
+  }
+
+  async exist() {
+    let existe = fs.existsSync(this.ruta);
+    return existe;
+  }
+
+  async read() {
+    let archivo = JSON.parse(await fs.promises.readFile(this.ruta, "utf-8"));
+    return archivo;
+  }
+
+  async save(producto, precio, foto) {
+    let archivo;
+    let existe = await this.exist();
+    if (existe) {
+      try {
+        archivo = await this.read();
+
+        let ultimo;
+        if (archivo.length < 1) ultimo = 1;
+        else {
+          ultimo = archivo[archivo.length - 1].id;
+        }
+        this.productos = [...archivo, { title: producto, price: precio, thumbnail: foto, id: ultimo + 1 }];
+
+        await fs.promises.writeFile(this.ruta, JSON.stringify(this.productos));
+        console.log("Producto cargado!");
+        return ultimo++;
+      } catch (err) {
+        console.log("Catch err: ---->", err);
+      }
+    } else console.log("No existe el archivo");
+  }
+
+  async getById(id_consultado) {
+    let existe = await this.exist();
+    if (existe) {
+      try {
+        let archivo = await this.read();
+        let producto_buscado = archivo.find((item) => item.id === id_consultado);
+        if (producto_buscado) return producto_buscado;
+        else return null;
+      } catch (err) {
+        console.log("Catch err: ---->", err);
+      }
+    } else console.log("No existe el archivo");
+  }
+
+  async getAll() {
+    let existe = await this.exist();
+    if (existe) {
+      try {
+        let archivo = await this.read();
+        return archivo;
+      } catch (err) {
+        console.log("Catch err: ---->", err);
+      }
+    } else console.log("No existe el archivo");
+  }
+
+  async deleteById(id_borrar) {
+    let existe = await this.exist();
+    if (existe) {
+      try {
+        let archivo = await this.read();
+        let producto_a_borrar = archivo.findIndex((item) => item.id === id_borrar);
+        if (producto_a_borrar) {
+          archivo.splice(producto_a_borrar, 1);
+          this.productos = [...archivo];
+          await fs.promises.writeFile(this.ruta, JSON.stringify(this.productos));
+          console.log("Producto borrado!");
+        } else console.log("No existe producto con el id indicado");
+      } catch (err) {
+        console.log("Catch err: ---->", err);
+      }
+    } else console.log("No existe el archivo");
+  }
+
+  async deleteAll() {
+    try {
+      this.productos = [];
+      await fs.promises.writeFile(this.ruta, JSON.stringify(this.productos));
+      console.log("Todos los datos eliminados");
+    } catch (err) {
+      console.log("Catch err: ---->", err);
+    }
+  }
+}
+
+const algo = async () => {
+  const producto_nuevo = new Contenedor("productos.txt");
+  await producto_nuevo.save("monopatin", 100, "moto.jpg");
+  console.log(await producto_nuevo.getById(6));
+  console.log("Trae todo", await producto_nuevo.getAll());
+  await producto_nuevo.deleteById(4);
+  await producto_nuevo.deleteAll();
+};
+
+algo();
